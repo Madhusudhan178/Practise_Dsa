a99be6cc25505a2c34bb15d2c1825fe3c8d10a0b
madhusudhan178 <madhusudhanmayur@gmail.com>
2026-04-06 10:21:53 +0530

    first commit

---
+++ C++/demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter the value of x:"<<endl;
+    cin>>x;
+    cout<<"Hello World!"<<endl;
+
+    
+    return 0;
+}
---
+++ C++/example.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int x=5;
+    double y=4.0;
+
+    printf(&y);
+    return 0;
+}
